53caa14dba9a359e2bedab15951c98cefeea637f
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2023-11-27 13:52:59 +0530

    Developed a grid of two columns by using topic grid item composable and added Top-Bottom Bar.

--- app/src/main/java/com/example/courseapp/MainActivity.kt
+++ app/src/main/java/com/example/courseapp/MainActivity.kt
@@ -4,27 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.courseapp.data.DataSource
 import com.example.courseapp.model.Topic
 import com.example.courseapp.ui.theme.CourseAppTheme
 
@@ -38,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    CourseApp()
                 }
             }
         }
@@ -46,7 +56,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TopicGridItem(topic: Topic,modifier: Modifier = Modifier) {
+fun TopicGridItem(topic: Topic, modifier: Modifier = Modifier) {
     Card(
         modifier = modifier
     ) {
@@ -87,10 +97,60 @@ fun TopicGridItem(topic: Topic,modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun TopicGridList(topics: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        items(topics) { topic ->
+            TopicGridItem(topic = topic)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CourseApp(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.DarkGray,
+                    titleContentColor = Color.White,
+                ),
+                title = {
+                    Text(stringResource(R.string.university_courses_text))
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = Color.DarkGray,
+                contentColor = Color.White,
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = stringResource(R.string.developer_name_text),
+                )
+            }
+        }
+    ) { it ->
+        TopicGridList(topics = DataSource.topics, modifier = Modifier
+            .fillMaxSize()
+            .padding(it)
+            .padding(8.dp))
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     CourseAppTheme {
-        TopicGridItem(topic = Topic(R.string.photography, 321, R.drawable.photography))
+        CourseApp()
     }
 }
