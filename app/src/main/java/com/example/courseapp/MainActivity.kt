17667a7fde482ac77f3941429c4c744acf90b920
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2023-11-27 13:17:07 +0530

    Created topic grid item composable.

--- app/src/main/java/com/example/courseapp/MainActivity.kt
+++ app/src/main/java/com/example/courseapp/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.courseapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courseapp.model.Topic
 import com.example.courseapp.ui.theme.CourseAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +46,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TopicGridItem(topic: Topic,modifier: Modifier = Modifier) {
+    Card(
         modifier = modifier
-    )
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = topic.imageResource),
+                contentDescription = stringResource(id = topic.courseName),
+                modifier = Modifier
+                    .size(68.dp)
+                    .fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = modifier.padding(top = 16.dp, end = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(id = topic.courseName),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(start = 16.dp, bottom = 8.dp)
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain_foreground),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = 16.dp, end = 8.dp)
+                            .size(20.dp)
+                    )
+                    Text(
+                        text = "${topic.numberOfCourses}",
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CourseAppTheme {
-        Greeting("Android")
+        TopicGridItem(topic = Topic(R.string.photography, 321, R.drawable.photography))
     }
 }
